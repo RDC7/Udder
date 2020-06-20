6f9e4b2f1bdd2cb4087aa74223a1b6516c75e5b1
RDC7 <ranaldrixson@hotmail.co.uk>
2020-06-20 19:33:16 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ranald Rixson on 20/06/2020.
+//  Copyright © 2020 Ranald Rixson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
